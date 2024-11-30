d57488ff56806acdf2621de46ca6edbf52b7c67f
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-11-30 11:08:01 -0800

    Update wsgi.py

--- appraisal_project/wsgi.py
+++ appraisal_project/wsgi.py
@@ -1,4 +1,11 @@
 import os
+import sys
+from pathlib import Path
+
+# Add the project root to the Python path
+project_root = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_root))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appraisal_project.settings')
